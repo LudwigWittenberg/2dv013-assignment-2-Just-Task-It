93f055f8ef2c1d444d9d5cedd85e53bb05000d74
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-28 13:25:59 +0100

    Try to deploy something to GKE
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

---
+++ k8s/gke/production/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type        = string
+  description = "The name of the project"
+}
+
+variable "region" {
+  type        = string
+  description = "The region of the project"
+}
+
+variable "google_project_id" {
+  type        = string
+  description = "The ID of the project"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the cluster"
+}
