a8845f392c457875c693210f0d32c4495e574e40
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-31 10:56:11 +0100

    Add UserController
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

---
+++ src/controllers/UserController.js
@@ -0,0 +1,204 @@
+/**
+ * AuthenticateController module.
+ *
+ * @author Ludwig Wittenberg <lw223cq@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import { UserModel } from "../models/UserModel.js";
+
+/**
+ * Encapsulates a controller.
+ */
+export class UserController {
+	/**
+	 * Encapsulates a controller.
+	 *
+	 * @param { req } req - Express request object.
+	 * @param { res } res - Express response object.
+	 * @param { next } next - Express next middleware function.
+	 */
+	login(req, res, next) {
+		try {
+			res.render("login/index");
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	/**
+	 * Encapsulates a controller.
+	 *
+	 * @param { req } req - Express request object.
+	 * @param { res } res - Express response object.
+	 * @param { next } next - Express next middleware function.
+	 */
+	async loginPost(req, res, next) {
+		try {
+			const user = await UserModel.authenticate(
+				req.body.username,
+				req.body.password,
+			);
+
+			req.session.regenerate((error) => {
+				if (error) {
+					throw new Error("Could not re-generate session");
+				}
+
+				req.session.user = user;
+
+				res.locals.user = user;
+
+				req.session.flash = {
+					type: "success",
+					text: "You are now logged in",
+				};
+
+				res.redirect("./");
+			});
+		} catch (error) {
+			console.log(error);
+			req.session.flash = {
+				type: "danger",
+				text: "Wrong username or password",
+			};
+			res.redirect("./login");
+		}
+	}
+
+	/**
+	 * Encapsulates a controller.
+	 *
+	 * @param { req } req - Express request object.
+	 * @param { res } res - Express response object.
+	 * @param { next } next - Express next middleware function.
+	 */
+	logout(req, res, next) {
+		try {
+			req.session.destroy((error) => {
+				if (error) {
+					throw new Error("Could not destroy session");
+				}
+
+				res.locals.user = null;
+
+				res.clearCookie(process.env.SESSION_NAME);
+
+				res.redirect("./");
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	/**
+	 * Encapsulates a controller.
+	 *
+	 * @param { req } req - Express request object.
+	 * @param { res } res - Express response object.
+	 * @param { next } next - Express next middleware function.
+	 */
+	register(req, res, next) {
+		try {
+			res.render("register/index");
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	/**
+	 * Encapsulates a controller.
+	 *
+	 * @param { req } req - Express request object.
+	 * @param { res } res - Express response object.
+	 * @param { next } next - Express next middleware function.
+	 * @returns { Promise } - A promise.
+	 */
+	async registerPost(req, res, next) {
+		try {
+			const { fullName, username, password, passwordRepeat } = req.body;
+
+			const existingUser = await UserModel.findOne({ username });
+
+			if (existingUser) {
+				req.session.flash = {
+					type: "danger",
+					text: "Username already exists",
+				};
+
+				return res.redirect("./register");
+			}
+
+			if (password.length < 8) {
+				req.session.flash = {
+					type: "danger",
+					text: "Password must be at least 8 characters long",
+				};
+
+				return res.redirect("./register");
+			}
+
+			if (password !== passwordRepeat) {
+				req.session.flash = {
+					type: "danger",
+					text: "Passwords do not match",
+				};
+
+				return res.redirect("./register");
+			}
+
+			await UserModel.create({
+				fullName,
+				username,
+				password,
+			});
+
+			req.session.flash = {
+				type: "success",
+				text: "Your account was successfully created",
+			};
+
+			res.redirect("./login");
+		} catch (_error) {
+			req.session.flash = {
+				type: "danger",
+				text: "There was an error while creating your account",
+			};
+			res.redirect("./register");
+		}
+	}
+
+	/**
+	 * Authorizes a user to access a page.
+	 *
+	 * @param { req } req - Express request object.
+	 * @param { res } res - Express response object.
+	 * @param { next } next - Express next middleware function.
+	 */
+	authorizeLogOut(req, res, next) {
+		if (!req.session.user) {
+			const error = new Error("Not Found");
+			error.status = 404;
+			next(error);
+		} else {
+			next();
+		}
+	}
+
+	/**
+	 * Authorizes a user to access a page.
+	 *
+	 * @param { req } req - Express request object.
+	 * @param { res } res - Express response object.
+	 * @param { next } next - Express next middleware function.
+	 */
+	authorizeLogIn(req, res, next) {
+		if (req.session.user) {
+			const error = new Error("Not Found");
+			error.status = 404;
+			next(error);
+		} else {
+			next();
+		}
+	}
+}
